7eb9642f57a5d16f5fc3db86e1507281bc21ac5b
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-04-26 00:00:00 +0530

    idententation maintained

--- index.php
+++ index.php
@@ -1,48 +1,58 @@
 <?php
+/**
+ * @Author: prabhakar
+ * @Date:   2016-04-26 13:58:12
+ * @Last Modified by:   Prabhakar Gupta
+ * @Last Modified time: 2016-04-28 11:56:13
+ */
+
 if(isset($_FILES['file'])){
-  $errors= "";
-  $error_trigger = 0;
-  $file_name = $_FILES['file']['name'];
-  $file_tmp =$_FILES['file']['tmp_name'];
+	$errors = "";
+	$error_trigger = 0;
+	$file_name = $_FILES['file']['name'];
+	$file_tmp = $_FILES['file']['tmp_name'];
 
-  if($_FILES["file"]["type"] !== "text/plain"){
-     $errors ="<p style=\"width:100%;height:30px;text-align:center\">Upload onother file, extension not allowed.</p>";
-  }
+	if($_FILES["file"]["type"] !== "text/plain"){
+		$errors ="<p style=\"width:100%;height:30px;text-align:center\">Upload onother file, extension not allowed.</p>";
+	}
 
-  if(empty($errors)==true){
-     move_uploaded_file($file_tmp, "conversations/" . $file_name);
-     header('Location: read.php?filename=' . $file_name);
-  } else {
-     $error_trigger = 1;
-  }
+	if(empty($errors)){
+		move_uploaded_file($file_tmp, "conversations/" . $file_name);
+		header('Location: read.php?filename=' . $file_name);
+	} else {
+		$error_trigger = 1;
+	}
 }
-    require 'inc/header.inc.php';
-    require 'inc/navbar.inc.php';
-    if(isset($_GET["error"]) && $_GET["error"] == 1)
-    {
-      echo "<p style=\"width:100%;height:30px;text-align:center\">No alterations with code Plz!</p>";
-    }
 
-    if(isset($error_trigger) && $error_trigger == 1)
-    {
-      echo($errors);
-    }
+require 'inc/header.inc.php';
+require 'inc/navbar.inc.php';
+
+if(isset($_GET["error"]) && $_GET["error"] == 1){
+	echo "<p style=\"width:100%;height:30px;text-align:center\">No alterations with code Plz!</p>";
+}
+
+if(isset($error_trigger) && $error_trigger == 1){
+	echo($errors);
+}
+
 ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="upload-popup">
-                    Upload Whatsapp Text (.txt) file and view chat in a readable format<br>
-                    <form action="" method="POST" enctype="multipart/form-data">
-                        <input type="file" name="file">
-                        <input type="submit" class="btn btn-warning" value="Get Conversation">
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="upload-popup">
+				Upload Whatsapp Text (.txt) file and view chat in a readable format<br>
+				<form action="" method="POST" enctype="multipart/form-data">
+					<input type="file" name="file">
+					<input type="submit" class="btn btn-warning" value="Get Conversation">
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php
-    require 'inc/footer.inc.php';
+
+require 'inc/footer.inc.php';
+
 ?>
