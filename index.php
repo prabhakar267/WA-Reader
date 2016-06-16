b5746bb27bafe654db77f2ef2cb9a940ebe3c927
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-06-16 23:34:01 +0530

    file processing complete

--- index.php
+++ index.php
@@ -111,24 +111,17 @@ $('form').on('submit', uploadFiles);
 
 var files;
 
-// Add events
 $('input[type=file]').on('change', prepareUpload);
 
-// Grab the files and set them to our variable
-function prepareUpload(event)
-{
-  files = event.target.files;
+function prepareUpload(event){
+	files = event.target.files;
 }
 
 // Catch the form submit and upload the files
-function uploadFiles(event)
-{
-  event.stopPropagation(); // Stop stuff happening
+function uploadFiles(event){
+	event.stopPropagation(); // Stop stuff happening
 	event.preventDefault(); // Totally stop stuff happening
 
-	// START A LOADING SPINNER HERE
-
-	// Create a formdata object and add the files
 	var data = new FormData();
 	$.each(files, function(key, value)
 	{
@@ -143,24 +136,12 @@ function uploadFiles(event)
 		dataType: 'json',
 		processData: false, // Don't process the files
 		contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-		success: function(data, textStatus, jqXHR)
-		{
-			if(typeof data.error === 'undefined')
-			{
-				// Success so call function to process the form
-				submitForm(event, data);
-			}
-			else
-			{
-				// Handle errors here
-				console.log('ERRORS: ' + data.error);
-			}
+		
+		success: function(response){
+			console.log(response);
 		},
-		error: function(jqXHR, textStatus, errorThrown)
-		{
-			// Handle errors here
+		error: function(jqXHR, textStatus, errorThrown){
 			console.log('ERRORS: ' + textStatus);
-			// STOP LOADING SPINNER
 		}
 	});
 }
--- upload-file.php
+++ upload-file.php
@@ -3,27 +3,36 @@
  * @Author: prabhakar
  * @Date:   2016-06-15 00:15:08
  * @Last Modified by:   Prabhakar Gupta
- * @Last Modified time: 2016-06-15 01:07:57
+ * @Last Modified time: 2016-06-16 23:30:28
  */
 
 require_once 'inc/function.inc.php';
 
 $api_url = "whatsapp-reader.php?filename=";
+$final_response = array();
+$error_flag = false;
+$errors = [];
 
-// $errors = "";
-// $error_trigger = 0;
 
-$file_name = $_FILES[0]['name'];
-$file_tmp = $_FILES[0]['tmp_name'];
+if(isset($_FILES[0]['type'])){
+	if($_FILES[0]['type'] != 'text/plain'){
+		add_error_message($errors, $error_flag, 'Invalid file type uploaded!');
+	} else {
+		move_uploaded_file($file_tmp, "conversations/" . $file_name);
 
-if($_FILES[0]["type"] !== "text/plain"){
-	$is_filetype_error = true;
-	// echo 'somethin';
-	// "<p style=\"width:100%;height:30px;text-align:center\">Upload onother file, extension not allowed.</p>";
+		$url = get_current_url(true) . $api_url . $file_name;
+		$response = whatsapp_reader($file_name);
+	}
 } else {
-	move_uploaded_file($file_tmp, "conversations/" . $file_name);
-	
-	$url = get_current_url(true) . $api_url . $file_name;
-	$response = whatsapp_reader($file_name);
-	echo json_encode($response, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+	add_error_message($errors, $error_flag, 'Invalid request!');
 }
+
+if(isset($response)){
+	$final_response = $response;
+} else {
+	$final_response['success'] = !$error_flag;
+	$final_response['errors'] 	= $errors;
+}
+
+
+echo json_encode($final_response, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
