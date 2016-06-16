f21e436914fd80555e6dff77a56a21faf2ace1b9
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-06-16 23:35:47 +0530

    jquery + bootstrap files add

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="prabhakar gupta">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>WhatsApp Reader | By Prabhakar Gupta</title>
 	<link rel="shortcut icon" href="img/whatsapp.png">
@@ -100,8 +100,8 @@
 	</div>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 
 
 
