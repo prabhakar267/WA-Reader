d359bc8ac39224dffa4c7d7f7f60aab5d5a0a3df
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2019-08-07 21:02:32 +0530

    Fix #45: DDMM date format is given preference over MMDD

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def _get_parsed_line(input_line, persons_list):
             dirty_timestamp_string = dirty_timestamp_string.replace(remove_character, "")
 
         try:
-            timestamp_string = parse_datetime(dirty_timestamp_string)
+            timestamp_string = parse_datetime(dirty_timestamp_string, dayfirst=True)
             line = timestamp_splitter.join(items[1:]).strip()
             break
         except ValueError:
