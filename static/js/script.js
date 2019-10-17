e48e0aa5e466a6e2569787a102f3d5e4b2b72c08
Ofem Eteng <ofemeteng@gmail.com>
2019-10-17 23:35:57 +0100

    script.js: Add error message alert
    This adds an error message alert to the page when an invalid txt file is provided
    
    Closes https://github.com/prabhakar267/WA-Reader/issues/60

--- static/js/script.js
+++ static/js/script.js
@@ -66,7 +66,14 @@ function uploadFiles(event){
                     last_user_index = chat_user_index;
                 }
             } else {
-                show_error_message(response.error_message);
+                $('.upload-popup').prepend('<div class="alert alert-danger alert-dismissible">' +
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+                response.error_message + 
+                '</div>');
+
+                setTimeout(function() {
+                    $('.alert').fadeOut('slow');
+                }, 4000);
             }
         },
         error: function(jqXHR, textStatus, errorThrown){
